78e5a92bc213850a4203f0b3953e715733e59706
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-08-18 15:30:26 +0200

    Fix typo

--- property.py
+++ property.py
@@ -9,7 +9,7 @@ from descriptor import *
 
 @dataclass(slots=True)
 class property[
-    X,             # the target type (subclasses can constrain thiss)
+    X,             # the target type (subclasses can constrain this)
     Tget = Never,  # the type returned by the getter
     Tset = Never,  # the type assignable via the setter
     Xdel = Never,  # either 'X' (if it is deletable) or 'Never' (if it isn't)
